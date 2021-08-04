6d234a5a885ac808dcb93bec2f2409e4bf41bbfc
Piotr Kasprzyk <ciri@ciri.pl>
2021-08-04 23:11:32 +0200

    flake8

--- common_pl.py
+++ common_pl.py
@@ -17,13 +17,27 @@ polskie_unicode = u'\u0105\u0107\u0119\u0142\u0144\xf3\u015b\u017c\u017a\u0104\u
 przykladowy_plik_zrodlowy = 'gen_plik.txt'
 przykladowy_plik_docelowy = 'gen_plik_out.txt'
 przykladowy_plik_trzeci = 'gen_plik_trzeci.txt'
+opis_opcji_programu = '''\
+Program do konwersji - Projekt AL (wersja Python)
+Opcje:
+i "plik"
+o "plik"
+io "plik" (odczyt i zapis do tego samego pliku)
+pl f1 f2
+No13 (usun CR)
+u8a (reczna podmiana polskich liter UTF8 -> ASCII)
+
+Formaty:
+%s'''
+
 
 # Skrot klawiszowy, format dla dekodowania, nazwa opisowa
 Formaty = (
-('iso', 'iso-8859-2', 'ISO-8859-2'),
-('utf8', 'utf-8', 'UTF-8'),
-('1250', 'cp1250', 'CP 1250'),
-)
+    ('iso', 'iso-8859-2', 'ISO-8859-2'),
+    ('utf8', 'utf-8', 'UTF-8'),
+    ('1250', 'cp1250', 'CP 1250'),
+    )
+
 
 def skrot_format():
     '''Slownik tlumaczacy skrot wpisany z klawiatury na polecenie dekodujace
@@ -32,6 +46,7 @@ def skrot_format():
     for skrot, format, nazwa in Formaty:
         wynik[skrot] = format
     return wynik
+
 
 class Parametry:
     '''Obsluga listy parametrow
@@ -44,13 +59,13 @@ class Parametry:
         self.konwersja_skrot_format = skrot_format()
         self.nazwa_do_zapisu = None
 
-    def sa_jeszcze_elementy(self, ile = 1):
+    def sa_jeszcze_elementy(self, ile=1):
         '''
         Parametry:
         '''
         return len(self.lista) >= ile
 
-    def pobierz(self, ile = 1):
+    def pobierz(self, ile=1):
         '''
         Parametry:
         '''
@@ -66,8 +81,7 @@ class Parametry:
             else:
                 raise RuntimeError('Nie podoba mi sie ilosc %d argumentow' % ile)
         else:
-            raise RuntimeError('Chce pobrac %d argumentow, a aktualna lista to: %s' %
-              (ile, repr(self.lista)))
+            raise RuntimeError('Chce pobrac %d argumentow, a aktualna lista to: %s' % (ile, repr(self.lista)))
 
     def pobierz_format(self):
         '''
@@ -93,13 +107,11 @@ class Parametry:
         if self.nazwa_do_zapisu is not None:
             dane.zapis_do_pliku(self.nazwa_do_zapisu)
 
+
 def informacja_obslugi_programu(wywolanie_testowe):
     if not wywolanie_testowe:
-        print(opis_opcji_programu % (
-      ''.join(
-        map(lambda a: '%s - %s\n' % (a[0], a[2]), Formaty)
-      )
-    ))
+        print(opis_opcji_programu % (''.join(map(lambda a: '%s - %s\n' % (a[0], a[2]), Formaty))))
+
 
 def wczytaj_tresc_pliku(nazwa):
     fd = open(nazwa, 'rb')
@@ -107,8 +119,8 @@ def wczytaj_tresc_pliku(nazwa):
     fd.close()
     return wynik
 
-class BuforDanych:
 
+class BuforDanych:
     def __init__(self):
         self.byl_odczyt = 0
         self.zerowanie()
@@ -138,7 +150,7 @@ class BuforDanych:
         return ''.join(wynik)
 
     def zabierz_dane(self):
-        if self.moja_tresc == None:
+        if self.moja_tresc is None:
             self.moja_tresc = self.wczytaj_stdin()
         wynik = self.moja_tresc
         self.zerowanie()
@@ -149,8 +161,9 @@ class BuforDanych:
 
     def zapis_do_pliku(self, nazwa_docelowego_pliku):
         fd = open(nazwa_docelowego_pliku, 'wb')
-        dane = fd.write(self.zabierz_dane())
+        fd.write(self.zabierz_dane())
         fd.close()
+
 
 def wczytanie_pliku_do_obiektu(dane, nazwa_pliku):
     if os.path.isfile(nazwa_pliku):
@@ -158,6 +171,7 @@ def wczytanie_pliku_do_obiektu(dane, nazwa_pliku):
         dane.wczytaj_plik(nazwa_pliku)
     else:
         raise RuntimeError('Brak pliku o nazwie "%s"' % nazwa_pliku)
+
 
 def klocek_odczyt(polecenia, dane):
     if polecenia.sa_jeszcze_elementy():
@@ -167,9 +181,11 @@ def klocek_odczyt(polecenia, dane):
         raise RuntimeError('Brak nazwy pliku dla opcji "i"')
     return nazwa_pliku
 
+
 def klocek_w_miejscu(polecenia, dane):
     nazwa_pliku = klocek_odczyt(polecenia, dane)
     polecenia.zapamietaj_do_zapisu(nazwa_pliku)
+
 
 def klocek_zapis(polecenia, dane):
     if polecenia.sa_jeszcze_elementy():
@@ -177,6 +193,7 @@ def klocek_zapis(polecenia, dane):
         dane.zapis_do_pliku(nazwa_pliku)
     else:
         raise RuntimeError('Brak nazwy pliku dla opcji "o"')
+
 
 def klocek_przekoduj(polecenia, dane):
     if polecenia.sa_jeszcze_elementy(2):
@@ -188,10 +205,12 @@ def klocek_przekoduj(polecenia, dane):
     else:
         raise RuntimeError('Potrzebuje dwoch nazw formatow dla opcji "pl"')
 
+
 def klocek_no13(dane):
     tmp = dane.zabierz_dane()
     tmp = tmp.replace('\r', '')
     dane.wstaw_dane(tmp)
+
 
 def klocek_utf8_to_ascii(dane):
     letter_pairs = zip(polskie_unicode, polskie_ascii)
@@ -200,6 +219,7 @@ def klocek_utf8_to_ascii(dane):
         src = konwersja_uni_utf8(src)
         tmp = tmp.replace(src, dst)
     dane.wstaw_dane(tmp)
+
 
 def obsluga_parametrow(polecenia, dane):
     while polecenia.sa_jeszcze_elementy():
@@ -220,12 +240,14 @@ def obsluga_parametrow(polecenia, dane):
             raise RuntimeError('Nierozpoznana opcja: %s' % repr(rozkaz))
     polecenia.opcjonalnie_zapisz_w_miejscu(dane)
 
-def Wykonaj(polecenia, wywolanie_testowe = 1):
+
+def Wykonaj(polecenia, wywolanie_testowe=1):
     dane = BuforDanych()
     if polecenia.sa_jeszcze_elementy():
         obsluga_parametrow(polecenia, dane)
     else:
         informacja_obslugi_programu(wywolanie_testowe)
+
 
 class Test_bledne_napisy(unittest.TestCase):
     def test_pusta_lista(self):
@@ -237,22 +259,26 @@ class Test_bledne_napisy(unittest.TestCase):
         else:
             raise RuntimeError('brakuje nazwy pliku dla opcji "i"')
 
+
 def konwersja_iso_uni(napis):
     return napis.decode('iso-8859-2')
+
 
 def konwersja_uni_iso(napis):
     return napis.encode('iso-8859-2')
 
+
 def konwersja_uni_utf8(napis):
     return napis.encode('utf-8')
+
 
 def konwersja_miedzy_formatami(napis, przed, po):
     napis = napis.decode(przed)
     napis = napis.encode(po)
     return napis
 
-class Test_nieistniejacy_plik(unittest.TestCase):
 
+class Test_nieistniejacy_plik(unittest.TestCase):
     def testWczytanie(self):
         try:
             Wykonaj(Parametry(['i', 'plik_nieistniejacy.txt']))
@@ -260,6 +286,7 @@ class Test_nieistniejacy_plik(unittest.TestCase):
             pass
         else:
             raise RuntimeError('udalo sie otworzyc nieistniejacy plik')
+
 
 def usun_plik_jesli_istnieje(nazwa):
     if os.path.isfile(nazwa):
@@ -301,11 +328,8 @@ class Test_istniejacy_plik(unittest.TestCase):
         else:
             raise RuntimeError('proba dwukrotnego odczytu z pliku')
         Wykonaj(Parametry(['i', self.nazwa_pliku_wej, 'o', self.nazwa_pliku_wyj]))
-        assert (
-        wczytaj_tresc_pliku(self.nazwa_pliku_wej)
-         ==
-        wczytaj_tresc_pliku(self.nazwa_pliku_wyj)
-        )
+        assert (wczytaj_tresc_pliku(self.nazwa_pliku_wej) == wczytaj_tresc_pliku(self.nazwa_pliku_wyj))
+
 
 class Test_konwersja_iso_utf8(unittest.TestCase):
 
@@ -330,48 +354,40 @@ class Test_konwersja_iso_utf8(unittest.TestCase):
 
     def test_wejscie_wyjscie(self):
         try:
-            Wykonaj(Parametry(
-              ('i %s pl o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
-            ))
+            Wykonaj(
+                Parametry(
+                    ('i %s pl o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
+                ))
         except RuntimeError:
             pass
         else:
             raise RuntimeError('przeszedlo zero argumentow dla opcji "pl"')
         try:
-            Wykonaj(Parametry(
-              ('i %s pl iso o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
-            ))
+            Wykonaj(
+                Parametry(
+                    ('i %s pl iso o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
+                ))
         except RuntimeError:
             pass
         else:
             raise RuntimeError('przeszedl jeden argument dla opcji "pl"')
-        Wykonaj(Parametry(
-          ('i %s pl iso utf8 o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
-        ))
-        Wykonaj(Parametry(
-          ('i %s pl utf8 iso o %s' % (self.nazwa_pliku_wyj, self.nazwa_pliku_trzeci)).split()
-        ))
-        assert (
-        wczytaj_tresc_pliku(self.nazwa_pliku_wej)
-         ==
-        wczytaj_tresc_pliku(self.nazwa_pliku_trzeci)
-        )
+        Wykonaj(
+            Parametry(
+                ('i %s pl iso utf8 o %s' % (self.nazwa_pliku_wej, self.nazwa_pliku_wyj)).split()
+            ))
+        Wykonaj(
+            Parametry(
+                ('i %s pl utf8 iso o %s' % (self.nazwa_pliku_wyj, self.nazwa_pliku_trzeci)).split()
+            ))
+        assert (wczytaj_tresc_pliku(self.nazwa_pliku_wej) == wczytaj_tresc_pliku(self.nazwa_pliku_trzeci))
 
-opis_opcji_programu = '''\
-Program do konwersji - Projekt AL (wersja Python)
-Opcje:
-i "plik"
-o "plik"
-io "plik" (odczyt i zapis do tego samego pliku)
-pl f1 f2
-No13 (usun CR)
-u8a (reczna podmiana polskich liter UTF8 -> ASCII)
-
-Formaty:
-%s'''
 
 def main():
     if len(sys.argv) == 2 and sys.argv[1] == 'test':
-        unittest.main(module='common_pl', argv = [sys.argv[0], ])
+        unittest.main(
+            module='common_pl',
+            argv=[
+                sys.argv[0],
+                ])
     else:
-        Wykonaj(Parametry(sys.argv[1:]), wywolanie_testowe = 0)
+        Wykonaj(Parametry(sys.argv[1:]), wywolanie_testowe=0)
