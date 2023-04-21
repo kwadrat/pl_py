0aa1d2b4c4ae08180c318ac4284239b8bb3136ee
Piotr Kasprzyk <ciri@ciri.pl>
2023-04-22 00:27:45 +0200

    Add ClaySpindle

--- common_pl.py
+++ common_pl.py
@@ -167,6 +167,95 @@ class BuforDanych:
         save_to_file(nazwa_docelowego_pliku, out_data)
 
 
+class ClaySpindle:
+    def __init__(self):
+        '''
+        ClaySpindle:
+        '''
+        self.state_first = 1
+
+    def is_first(self):
+        '''
+        ClaySpindle:
+        '''
+        return self.state_first
+
+    def klocek_odczyt(self, polecenia, dane):
+        '''
+        ClaySpindle:
+        '''
+        if self.state_first:
+            self.state_first = 0
+            if polecenia.sa_jeszcze_elementy():
+                nazwa_pliku = polecenia.pobierz()
+                wczytanie_pliku_do_obiektu(dane, nazwa_pliku)
+            else:
+                raise RuntimeError('Brak nazwy pliku dla opcji "i"')
+        else:
+            raise RuntimeError('Ta opcja nie jest na poczatku lancucha: "i"')
+        return nazwa_pliku
+
+    def klocek_w_miejscu(self, polecenia, dane):
+        '''
+        ClaySpindle:
+        '''
+        if self.state_first:
+            self.state_first = 0
+            nazwa_pliku = klocek_odczyt(polecenia, dane)
+            polecenia.zapamietaj_do_zapisu(nazwa_pliku)
+        else:
+            raise RuntimeError('Ta opcja nie jest na poczatku lancucha: "io"')
+
+    def klocek_zapis(self, polecenia, dane):
+        '''
+        ClaySpindle:
+        '''
+        if polecenia.sa_jeszcze_elementy():
+            nazwa_pliku = polecenia.pobierz()
+            dane.zapis_do_pliku(nazwa_pliku)
+        else:
+            raise RuntimeError('Brak nazwy pliku dla opcji "o"')
+
+    def klocek_przekoduj(self, polecenia, dane):
+        '''
+        ClaySpindle:
+        '''
+        if polecenia.sa_jeszcze_elementy(2):
+            format_przed = polecenia.pobierz_format()
+            format_po = polecenia.pobierz_format()
+            tmp = dane.zabierz_dane()
+            tmp = konwersja_miedzy_formatami(tmp, format_przed, format_po)
+            dane.wstaw_dane(tmp)
+        else:
+            raise RuntimeError('Potrzebuje dwoch nazw formatow dla opcji "pl"')
+
+    def klocek_no13(self, dane):
+        '''
+        ClaySpindle:
+        '''
+        tmp = dane.zabierz_dane()
+        tmp = tmp.replace(b'\r', b'')
+        dane.wstaw_dane(tmp)
+
+    def klocek_utf8_to_ascii(self, dane):
+        '''
+        ClaySpindle:
+        '''
+        letter_pairs = zip(polskie_unicode, polskie_ascii)
+        tmp = dane.zabierz_dane()
+        if ix_dia.three_or_more:
+            tmp = tmp.decode(ix_dia.et_cdng_eight_utf)
+        for src, dst in letter_pairs:
+            if ix_dia.three_or_more:
+                pass
+            else:
+                src = ix_dia.konwersja_uni_utf8(src)
+            tmp = tmp.replace(src, dst)
+        if ix_dia.three_or_more:
+            tmp = tmp.encode(ix_dia.et_cdng_eight_utf)
+        dane.wstaw_dane(tmp)
+
+
 def wczytanie_pliku_do_obiektu(dane, nazwa_pliku):
     if os.path.isfile(nazwa_pliku):
         # Wczytaj calosc pliku
